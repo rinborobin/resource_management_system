0b3bfc168df00620764bb2a048231ef7c5fe3245
Robin <rinborobin629@gmail.com>
2026-06-07 15:29:04 +0700

    task assignment

--- main.c
+++ main.c
@@ -8,48 +8,8 @@ Library lib = {0};
 
 int main()
 {
-
+    initLibrary(&lib);
     mainMenu(&lib);
-
-    return 0;
-    // bool isRunning = true;
-    // printf("\n == Library Management System ==\n");
-    // while (isRunning)
-    // {
-    //     int choice;
-    //     printf("1. Book Management\n");
-    //     printf("2. Member Management\n");
-    //     printf("3. Borrow Book\n");
-    //     printf("4. Return Book\n");
-    //     printf("5. Report\n");
-    //     printf("6. Exit\n");
-    //     printf("Enter your choice: ");
-    //     scanf("%d", &choice);
-
-    //     switch (choice)
-    //     {
-    //     case 1:
-    //         // Book Management
-    //         break;
-    //     case 2:
-    //         // Member Management
-    //         break;
-    //     case 3:
-    //         // Borrow Book
-    //         break;
-    //     case 4:
-    //         // Return Book
-    //         break;
-    //     case 5:
-    //         // Report
-    //         break;
-    //     case 6:
-    //         isRunning = false;
-    //         break;
-    //     default:
-    //         printf("Invalid choice. Please try again.\n");
-    //     }
-    // }
 
     return 0;
 }
--- member.c
+++ member.c
@@ -1,7 +1,17 @@
 #include "book.h"
 #include <stdio.h>
-void memberMenu(Library *lib) {};
-void addMember(Library *lib) {};
-void viewMembers(Library *lib) {};
-void updateMember(Library *lib) {};
-void removeMember(Library *lib) {};
+void memberMenu(Library *lib) // Ly Sievminh
+{
+}
+void addMember(Library *lib)
+{
+}
+void viewMembers(Library *lib) // Ly Sievmiinh
+{
+}
+void updateMember(Library *lib)
+{
+}
+void removeMember(Library *lib)
+{
+}
