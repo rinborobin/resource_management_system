ff241ce3f59c37ebf4476dfb5b945da769b6c565
Robin <rinborobin629@gmail.com>
2026-06-12 14:10:58 +0700

    clear struct comment

--- member.c
+++ member.c
@@ -4,52 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// #ifndef DATA_H
-// #define DATA_H
-
-// typedef struct
-// {
-//     int book_id;
-//     char title[100];
-//     char author[100];
-//     int quantity;
-//     int available;
-// } Book;
-
-// typedef struct
-// {
-//     int member_id;
-//     char name[100];
-// } Member;
-
-// typedef struct
-// {
-//     int borrow_id;
-//     int book_id;
-//     int member_id;
-//     int returned;
-// } BorrowRecord;
-
-// typedef struct
-// {
-//     Book *books;
-//     int book_capacity;
-//     int book_count;
-//     int next_book_id;
-
-//     Member *members;
-//     int member_capacity;
-//     int member_count;
-//     int next_member_id;
-
-//     BorrowRecord *records;
-//     int record_capacity;
-//     int record_count;
-//     int next_record_id;
-// } Library;
-
-// #endif
-
 Member inputMember(Library *lib, bool is_update)
 {
     getchar();
