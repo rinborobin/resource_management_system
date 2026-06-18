0d6f013da20218ae072d7104fce704a733f1acc1
Robin <rinborobin629@gmail.com>
2026-06-18 11:53:09 +0700

    clean comment function

--- member.c
+++ member.c
@@ -66,14 +66,6 @@ int searchMember(Library *lib, int member_id)
     return -1;
 }
 
-// void toLowerString(char str[])
-// {
-//     for (int i = 0; str[i] != '\0'; i++)
-//     {
-//         str[i] = tolower((unsigned char)str[i]);
-//     }
-// }
-
 void viewMembers(Library *lib, int index) // Ly Sievminh
 {
     printf("ID: %d, Name: %s\n", lib->members[index].member_id, lib->members[index].name);
